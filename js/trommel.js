9aa650d3fdfb5c7608ba834fcd5de0bd9ce9fbc8
Ramakrishna <ramakrishnathehead@gmail.com>
2022-09-27 12:17:13 +0530

    trommel iv2

--- js/trommel.js
+++ js/trommel.js
@@ -1,78 +1,106 @@
 function calc() {
-    var M2 = parseFloat(document.getElementById("M2").value);
-    var M4 = parseFloat(document.getElementById("M4").value);
-    var M6 = parseFloat(document.getElementById("M6").value);
-    var M8 = parseFloat(document.getElementById("M8").value);
-  
     var output = document.getElementById("output");
-    var gtable = document.getElementById("gtable");
+    var atable = document.getElementById("atable");
   
     var htmlres = "";
-    var htmlgt = "";
-  
-    var data = [];
-  
-    data.push({
-      Time: 2,
-      Cum_Time: 2,
-      Mass: M2,
-      Cum_Mass: 0,
-      Cum_Mass_Percent: 0,
+    var htmlat = "";
+
+    var predata = [];
+    predata.push({
+      MeshNo: 4,
+      MeshSize: 4.00,
     });
-  
-    data.push({
-      Time: 2,
-      Cum_Time: 4,
-      Mass: M4,
-      Cum_Mass: 0,
-      Cum_Mass_Percent: 0,
+
+    predata.push({
+      MeshNo: 5,
+      MeshSize: 3.35,
     });
-  
-    data.push({
-      Time: 2,
-      Cum_Time: 6,
-      Mass: M6,
-      Cum_Mass: 0,
-      Cum_Mass_Percent: 0,
+
+    predata.push({
+      MeshNo: 6,
+      MeshSize: 2.80,
     });
-  
-    data.push({
-      Time: 2,
-      Cum_Time: 8,
-      Mass: M8,
-      Cum_Mass: 0,
-      Cum_Mass_Percent: 0,
+
+    predata.push({
+      MeshNo: 7,
+      MeshSize: 2.36,
     });
-  
-    var Total_Material = 0;
-    for (i = 0; i <= data.length - 1; i++) {
-      Total_Material += data[i].Mass;
-      data[i].Cum_Mass = Total_Material;
-      data[i].Cum_Mass_Percent = (data[i].Cum_Mass / 500) * 100;
+
+    predata.push({
+      MeshNo: 8,
+      MeshSize: 2.00,
+    });
+
+    predata.push({
+      MeshNo: 12,
+      MeshSize: 1.00,
+    });
+
+    predata.push({
+      MeshNo: "pan",
+      MeshSize: "---",
+    });
+
+
+    var mdata = [];
+    var tmf=0;
+    var tmo=0;
+    var tmu=0;
+    for (i=1; i<=7; i++){
+      var mf = parseFloat(document.getElementById("f"+i).value);
+      var mo = parseFloat(document.getElementById("o"+i).value);
+      var mu = parseFloat(document.getElementById("u"+i).value);
+      mdata.push(
+        {
+          M_f: mf,
+          M_o: mo,
+          M_u: mu,
+        }
+      );
+      tmf += mf;
+      tmo += mo;
+      tmu += mu;
     }
   
-    var m =
-      (data[3].Cum_Mass_Percent - data[2].Cum_Mass_Percent) /
-      (data[3].Cum_Time - data[2].Cum_Time);
-    var c = data[3].Cum_Mass_Percent - m * data[3].Cum_Time;
-    var y80 = 80;
-    var x80 = (y80 - c) / m;
+    var mfdata = [];
+
+    var cmfdata = [];
+    var tf4cmf = 0;
+    var to4cmf = 0;
+    var tu4cmf = 0;
+
+    for (i=0; i<7; i++){
+      mfdata.push({
+        Mf_f: mdata[i].M_f/tmf,
+        Mf_o: mdata[i].M_o/tmo,
+        Mf_u: mdata[i].M_u/tmu,
+      });
+
+      tf4cmf += mfdata[i].Mf_f;
+      to4cmf += mfdata[i].Mf_o;
+      tu4cmf += mfdata[i].Mf_u;
+      cmfdata.push({
+        Cmf_f: tf4cmf,
+        Cmf_o: to4cmf,
+        Cmf_u: tu4cmf,
+      });
+    }
   
-    htmlgt += "<tr>";
-    htmlgt += "<th>Time (min)</th>";
-    htmlgt += "<th>Cumulative Time</th>";
-    htmlgt += "<th>Mass of -52 Fraction</th>";
-    htmlgt += "<th>Cumulative Mass of -52 fraction</th>";
-    htmlgt += "<th>Cumulative Mass percent of -52 fraction</th>";
-    htmlgt += "</tr>";
+    htmlat += "<tr>";
+    htmlat += "<th>Time (min)</th>";
+    htmlat += "<th>Cumulative Time</th>";
+    htmlat += "<th>Mass of -52 Fraction</th>";
+    htmlat += "<th>Cumulative Mass of -52 fraction</th>";
+    htmlat += "<th>Cumulative Mass percent of -52 fraction</th>";
+    htmlat += "</tr>";
     for (i = 0; i <= data.length - 1; i++) {
-      htmlgt += "<tr>";
-      htmlgt += "<td>" + data[i].Time + "</td>";
-      htmlgt += "<td>" + data[i].Cum_Time + "</td>";
-      htmlgt += "<td>" + data[i].Mass + "</td>";
-      htmlgt += "<td>" + data[i].Cum_Mass + "</td>";
-      htmlgt += "<td>" + data[i].Cum_Mass_Percent.toFixed(2) + "</td>";
-      htmlgt += "</tr>";
+      htmlat += "<tr>";
+      htmlat += "<td>" + data[i].Time + "</td>";
+      htmlat += "<td>" + data[i].Cum_Time + "</td>";
+      htmlat += "<td>" + data[i].Mass + "</td>";
+      htmlat += "<td>" + data[i].Cum_Mass + "</td>";
+      htmlat += "<td>" + data[i].Cum_Mass_Percent.toFixed(2) + "</td>";
+      htmlat += "</tr>";
     }
   
     if (!M2 || !M4 || !M6 || !M8) {
@@ -85,10 +113,10 @@ function calc() {
         x80.toFixed(3) +
         " min";
     }
-    var glabel = document.getElementById("glabel");
-    glabel.innerText = "Observation Table";
+    var alabel = document.getElementById("alabel");
+    alabel.innerText = "Observation Table";
   
-    gtable.innerHTML = htmlgt;
+    atable.innerHTML = htmlat;
     output.innerHTML = htmlres;
   
     parent.location = "#results";
