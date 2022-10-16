85fb1e7a25e3bdf3d0ac54249931758c5e580f02
Ramakrishna <ramakrishnathehead@gmail.com>
2022-10-16 22:17:10 +0530

    counter flow calc fixed, units added

--- js/counterflow.js
+++ js/counterflow.js
@@ -19,17 +19,18 @@ function calc() {
   }
 
   var cp = 4.1868;
-  var qh = Mh * 0.001 * cp * (T1 - T2);
-  var qc = Mc * 0.001 * cp * (T4 - T3);
+  var qh = Mh * 0.001 * cp * Math.abs(T1 - T2);
+  var qc = Mc * 0.001 * cp * Math.abs(T3 - T4);
   var q = (qh + qc) / 2;
   var del_T1 = T1 - T3;
   var del_T2 = T2 - T4;
-  var LMTD = del_T1 - del_T2;
+  var LMTD = (del_T1 - del_T2)/Math.log(del_T1/del_T2);
   var A = Math.PI * 0.015 * 1.8;
   var U = q / (A * LMTD);
+  U = U*1000;
 
   var result = document.getElementById("result");
-  result.innerHTML = "<b>Result: </b><br>Overall Heat Transfer Coefficient:  " + U.toFixed(3);
+  result.innerHTML = "<b>Result: </b><br>Overall Heat Transfer Coefficient:  " + U.toFixed(3) + "W/m<sup>2</sup>K";
 
   parent.location = "#output";
   document.getElementById("result").style.display = "none";
